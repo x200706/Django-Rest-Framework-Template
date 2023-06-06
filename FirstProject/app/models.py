2913fc07b40661bf3eeb4cfe9fb1ead48bf8e31f
Crystal <chi200706@gmail.com>
2023-06-06 21:48:18 +0800

    add a table, serializer,view and url config

--- FirstProject/app/models.py
+++ FirstProject/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     user_name = models.CharField(primary_key=True, max_length=15)
---
+++ FirstProject/app/serializers.py
@@ -0,0 +1,8 @@
+from models import User  # 我只撿了model裡面的一個小傢伙User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
